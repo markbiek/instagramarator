53ff0d1c274ba0aba6dc362454115dbb66c54fd7
Mark Biek <support@janustech.net>
2016-01-04 16:20:16 -0500

    Coding standards updates

--- background.js
+++ background.js
@@ -1,31 +1,15 @@
 console.log('background.js');
 
 var g = {
-    download: function(opts) {
+    download: function (opts) {
         console.log(opts);
     }
 };
 
-chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
+chrome.extension.onRequest.addListener(function (req, sender, sendResponse) {
     console.log('onRequest');
 
-    if(g.hasOwnProperty(req.action)) {
+    if (g.hasOwnProperty(req.action)) {
         g[req.action].call(req.opts);
     }
-
-    /*if(req.greeting == "hello") {
-        console.log(req);
-        console.log(sender);
-        var1 = req.var1; // Set variable 1
-        var2 = req.var2; // Set variable 2
-
-        chrome.tabs.getSelected(null, function(tab) {
-            chrome.tabs.sendRequest(tab.id, {method: 'ping'}, function(response) {
-                console.log(response.data);
-            });
-        });
-        
-    } else {
-        sendResponse({});    // Stop
-    }*/
 });
--- inject.js
+++ inject.js
@@ -1,9 +1,9 @@
-(function($) {
-    var kickoffDownload = function(url) {
-        if(url != undefined) {
+(function ($) {
+    var kickoffDownload = function (url) {
+        if (url != undefined) {
             var html = '<img src="' + url + '" width="76px" height="76px" alt="" />';
 
-            var div = $('<div />').css( {
+            var div = $('<div />').css({
                 'position': 'fixed',
                 'top': '0px',
                 'right': '16px',
@@ -11,45 +11,49 @@
                 'height': '76px',
                 'overflow': 'hidden',
                 'z-index': '100000',
-                'border': '3px solid #ccc'   
+                'border': '3px solid #ccc'
             });
 
             $(div).attr('id', 'instagramarator');
             $(div).html(html);
             $('body').append(div);
-        }else {
+        } else {
             console.error('Invalid undefined url passed to kickoffDownload');
         }
     };
 
-    var profileLinkExists = function() {
+    var profileLinkExists = function () {
         return $('.-cx-PRIVATE-ProfilePage__username').length > 0;
     };
 
-    var findImageAndDownload = function() {
+    var findImageAndDownload = function () {
         var img = findImage();
 
-        if(img != '') {
+        if (img != '') {
             kickoffDownload(img);
         }
     };
 
-    var findImage = function() {
+    var findImage = function () {
         var $image = $('.-cx-PRIVATE-Photo__image');
-    
-        if($image.length > 0) {
+
+        if ($image.length > 0) {
             return $image.attr('src');
         }
 
         return '';
     };
 
-    $(document).ready( function() {
-        $(document).on('mouseenter', '.-cx-PRIVATE-PostsGridItem__root', function(e) {
+    $(document).ready(function () {
+        $(document).on('mouseenter', 'a', function (e) {
+            console.log(this);
+        });
+
+        $(document).on('mouseenter', '.-cx-PRIVATE-PostsGridItem__root', function (e) {
             var imgSrc = $('img', this).attr('src');
             console.log(imgSrc);
 
-            var $img = $('<img />').css( {
+            var $img = $('<img />').css({
                 'z-index': 1000,
                 'display': 'block',
                 'position': 'absolute',
@@ -63,11 +67,11 @@
             $('.-cx-PRIVATE-PostsGridItem__postInfo').append($img);
         });
 
-        $(document).on('mouseleave', '.-cx-PRIVATE-PostsGridItem__root,.-cx-PRIVATE-PostsGridItem__postInfo', function(e) {
+        $(document).on('mouseleave', '.-cx-PRIVATE-PostsGridItem__root,.-cx-PRIVATE-PostsGridItem__postInfo', function (e) {
         });
 
         /*
-        if(!profileLinkExists()) {
+        if (!profileLinkExists()) {
             findImageAndDownload();
         }
         */
